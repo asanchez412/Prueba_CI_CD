0071d9e21f3fae9f1769758ea573b8318bd23a6e
asanchez497 <andres.sanchez@mercadolibre.com>
2023-08-29 19:23:54 -0300

    added dummy test to test the ci pipeline

---
+++ src/test/java/com/prueba/unit/controller/BasicControllerTest.java
@@ -0,0 +1,17 @@
+package com.prueba.unit.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class BasicControllerTest {
+    @Test
+    public void StubTest() {
+        var stubVariable = 1;
+        var stubExpected = 1;
+        assertEquals(stubExpected, stubVariable);
+    }
+}
